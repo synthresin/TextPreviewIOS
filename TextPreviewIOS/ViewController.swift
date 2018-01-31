196b1466b475f44c2175bff7857abc02fb73955a
ben <ben@frograms.com>
2018-01-31 13:14:14 +0900

    Initial Commit

---
+++ TextPreviewIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextPreviewIOS
+//
+//  Created by Seongwan Park on 2018. 1. 31..
+//  Copyright © 2018년 Seongwan Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
